b0c6908696bcb704254c9f053a55f87e24dd5a82
EC2 Default User <ec2-user@ip-172-31-26-59.ap-northeast-1.compute.internal>
2022-09-19 10:20:02 +0000

    変更

--- db/migrate/20220907064505_create_post_tags.rb
+++ db/migrate/20220907064505_create_post_tags.rb
@@ -1,8 +1,8 @@
 class CreatePostTags < ActiveRecord::Migration[6.1]
   def change
     create_table :post_tags do |t|
-      t.references  :post, foreign_key: true
-      t.references  :tag,  foreign_key: true
+      t.references  :post, foreign_key: true, type: :integer
+      t.references  :tag,  foreign_key: true, type: :integer
       t.timestamps
     end
     # 同じタグを２回保存するのはできないようになる
--- db/migrate/20220916090838_create_messages.rb
+++ db/migrate/20220916090838_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
-      t.references :customer, null: false, foreign_key: true
-      t.references :room,     null: false, foreign_key: true
+      t.references :customer, null: false, foreign_key: true, type: :integer
+      t.references :room,     null: false, foreign_key: true, type: :integer
       t.text       :content
       t.timestamps
     end
